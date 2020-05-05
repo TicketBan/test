de5cb216b6684805f9452efd4abb01d59a3e49cc
TicketBan <35073338+TicketBan@users.noreply.github.com>
2020-05-05 16:58:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_POST["send"])){
+    print_r($_POST)
+
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Обратная связь</title>
+</head>
+<body>
+    <h2>Forma</h2>
+    <form name="feedbeck" action="" method="post">
+    	<label>От кого</label><br/>
+    	<input type="text" name="from"/><br/>
+    	<label>Кому </label><br/>
+    	<input type="text" name="from"/><br/>
+    	<label>Тема</label><br/>
+    	<input type="text" name="from"/><br/>
+    	<label>Сообщение</label><br/>
+    	<textarea name="massege" cols="20" rows="20"></textarea><br/>
+    	<input type="submit" name="send" value="Отправить">
+     </form>
+</body>
+</html>
